06afb5d93be0069cf4315283cfab66216a6966e1
Gustavo Canello <gustavobrandicanello@gmail.com>
2023-08-07 17:42:26 -0300

    minor fix

--- gustavo/modules/replayGame.js
+++ gustavo/modules/replayGame.js
@@ -3,7 +3,7 @@ import { ui } from "./ui.js";
 export function replayGame(game) {
   const playAgain = ui.getPlayerPlayAgainInput()?.toLowerCase();
   if (playAgain === "yes") game();
-  else if (playAgain === "no") return ui.end();
+  else if (playAgain === "no") return ui.logEnd();
   else {
     ui.alertYesOrNo();
     replayGame(game);
