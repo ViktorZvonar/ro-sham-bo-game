ffd5af6856932ce0fefdc3b186f65398ba416faa
RAJ KUMAR THAPA <73226546+Raazk10@users.noreply.github.com>
2023-08-07 09:58:02 +0200

    Add game solution

---
+++ raj-solution/modules/gameRules.js
@@ -0,0 +1,9 @@
+export default function GameRules() {
+  alert("Welcome to Rock, Paper & Scissors game");
+  alert("Rule 1: Paper beats Rock!");
+  alert("Rule 2: Rock beats Scissors!");
+  alert("Rule 3: Scissors beats Paper!");
+  alert("Rule 4: If same choice it's a Draw!");
+  alert("Rule 5: If other choice than mentioned above it's a lose!");
+  alert("Best of luck! Start playing game");
+}
