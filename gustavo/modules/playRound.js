e6a1cbe0f7bf6ac1c3af1ea70795707824ee2312
Gustavo Canello <gustavobrandicanello@gmail.com>
2023-08-09 08:01:16 -0300

    cleaned up, added scoreboard icons

--- gustavo/modules/playRound.js
+++
@@ -1,7 +0,0 @@
-import { calculateRoundResult } from "./calculateRoundResult.js";
-import { applyRoundResult } from "./applyRoundResult.js";
-
-export function playRound(playerSelection, computerSelection) {
-  const roundResult = calculateRoundResult(playerSelection, computerSelection);
-  applyRoundResult(roundResult, playerSelection, computerSelection);
-}
